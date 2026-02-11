acaaef4857925cba0e718bb01a57781955bbc166
PTOM76 <ptms76@outlook.jp>
2026-02-11 16:35:00 +0900

    fix for 1.21.11 neoforge

--- forge/src/main/java/net/pitan76/legacyitemmodels/forge/LegacyItemmodelsForge.java
+++ forge/src/main/java/net/pitan76/legacyitemmodels/forge/LegacyItemmodelsForge.java
@@ -1,5 +1,6 @@
 package net.pitan76.legacyitemmodels.forge;
 
+import net.minecraftforge.eventbus.api.bus.BusGroup;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.pitan76.legacyitemmodels.LegacyItemmodels;
 import net.minecraftforge.fml.common.Mod;
@@ -7,8 +8,9 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(LegacyItemmodels.MOD_ID)
 public class LegacyItemmodelsForge {
-    public LegacyItemmodelsForge() {
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::onSetup);
+    public LegacyItemmodelsForge(FMLJavaModLoadingContext context) {
+        BusGroup busGroup = context.getModBusGroup();
+        FMLCommonSetupEvent.getBus(busGroup).addListener(this::onSetup);
     }
 
     public void onSetup(FMLCommonSetupEvent e) {
