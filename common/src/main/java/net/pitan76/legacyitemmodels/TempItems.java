6f263adcf8d8a79f54c94af9950e7a7812eee0ad
PTOM76 <ptms76@outlook.jp>
2026-03-18 17:25:08 +0900

    fix: https://github.com/Pitan76/MCPitanLib/issues/62

--- common/src/main/java/net/pitan76/legacyitemmodels/TempItems.java
+++ common/src/main/java/net/pitan76/legacyitemmodels/TempItems.java
@@ -3,14 +3,15 @@ package net.pitan76.legacyitemmodels;
 import net.minecraft.registry.Registries;
 import net.minecraft.util.Identifier;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class TempItems {
-    public static List<Identifier> items = new ArrayList<>();
+    public static final Set<Identifier> items = ConcurrentHashMap.newKeySet();
 
     static {
         Registries.ITEM.getIds().forEach(id -> {
+            if (id == null) return;
             if (id.getNamespace().equals("minecraft")) return;
 
             items.add(id);
